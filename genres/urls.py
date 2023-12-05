32fb93e4920d260b718f66a5ab03a9b71845f372
wandryell <wandryell@gmai.com>
2023-12-05 12:08:13 -0400

    refactor genre view, adjust UUID validation

--- genres/urls.py
+++ genres/urls.py
@@ -5,5 +5,5 @@ from .views import GenreListCreateAPIView, GenreRetrieveUpdateDestroyAPIView
 
 urlpatterns = [
     path('genres/', GenreListCreateAPIView.as_view(), name='genre-create-list'),
-    path('genre/<str:genre_uuid>', GenreRetrieveUpdateDestroyAPIView.as_view(), name='genre-detail')
+    path('genre/<int:pk>', GenreRetrieveUpdateDestroyAPIView.as_view(), name='genre-detail')
 ]
--- genres/views.py
+++ genres/views.py
@@ -1,12 +1,7 @@
-from django.http import Http404
 from rest_framework import generics
-from rest_framework.response import Response
-from rest_framework.validators import ValidationError
 
 from .models import Genre
 from .serializers import GenreSerializer
-
-import uuid
 
 
 class GenreListCreateAPIView(generics.ListCreateAPIView):
@@ -17,27 +12,3 @@ class GenreListCreateAPIView(generics.ListCreateAPIView):
 class GenreRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
-    
-    def get_object(self):
-        genre_uuid = self.kwargs.get('genre_uuid')
-        
-        try:
-            valid_uuid = uuid.UUID(genre_uuid)
-        except ValueError:
-            raise ValidationError('gênero não encontrado')
-            
-        try:
-            genre = Genre.objects.get(pk=genre_uuid)
-            return genre
-            
-        except Genre.DoesNotExist:
-            raise Http404('gênero não encontrado')
-        
-    def get(self, *args, **kwargs):
-        try: 
-            genre = self.get_object()
-            serializer = GenreSerializer(genre)
-            return Response(serializer.data)
-        
-        except Http404 as e:
-            return Response({'detail': 'gênero não encontrado.'})
