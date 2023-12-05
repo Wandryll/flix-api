df762f76c5c97574c19f641916292c635ca2ba4e
wandryell <wandryell@gmai.com>
2023-12-05 12:07:27 -0400

    Revert "modify field in the Genre model's id, Class Meta"
    This reverts commit a41c57166b2f340a98fe3a7dec42c1ce91137b46.

--- genres/models.py
+++ genres/models.py
@@ -1,14 +1,9 @@
 from django.db import models
 
-import uuid
 
 
 class Genre(models.Model):
-    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
     name = models.CharField(max_length=200, unique=True)
     
     def __str__(self):
         return self.name
-    
-    class Meta:
-        db_table = 'genres'
